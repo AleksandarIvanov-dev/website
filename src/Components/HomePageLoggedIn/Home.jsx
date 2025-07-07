a44726e61d5f724449bc7d451ecbddb200dede25
AleksandarIvanov-dev <s203081@stud.uni-ruse.bg>
2025-07-07 20:54:43 +0300

    Added chatbot to main home page. Will be removed

--- src/Components/HomePageLoggedIn/Home.jsx
+++ src/Components/HomePageLoggedIn/Home.jsx
@@ -3,10 +3,12 @@ import HomePageHeader from "./HomePageHeader";
 import TutorialList from "./TutorialList";
 
 export default function HomePage() {
-return (
+    return (
         <div className="min-h-screen bg-gray-100">
-            <HomePageHeader/>
+            <HomePageHeader />
             <main className="max-w-4xl mx-auto p-6">
+                <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                <div class="elfsight-app-30425757-dbb3-4271-9a6c-9b0de79fc125" data-elfsight-app-lazy></div>
                 <TutorialList />
             </main>
         </div>
