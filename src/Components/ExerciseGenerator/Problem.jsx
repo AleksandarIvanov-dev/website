4eedd10faa1947b3c2c2cfbec164268db26b0839
AleksandarIvanov-dev <s203081@stud.uni-ruse.bg>
2025-07-07 15:26:56 +0300

    Add initial Problem component structure with imports

---
+++ src/Components/ExerciseGenerator/Problem.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import HomePageHeader from "../HomePageLoggedIn/HomePageHeader";
+import FooterHomePage from "../HomePage/FooterHomePage";
+
+export default function Problem() {
+}
